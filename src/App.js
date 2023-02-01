1a2efa06d4b071bf785d74c14fd274f659ff803c
vardy.ng <vardy.ng@ecloudvalley.com>
2023-02-01 16:47:42 +0800

    update title and Url, remove protip

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
-import ProTip from './ProTip';
 
 function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://mui.com/">
-        Your Website
+        azure-poc-shopping-list.wadeenghoiwa.com
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
@@ -23,9 +22,8 @@ export default function App() {
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
         <Typography variant="h4" component="h1" gutterBottom>
-          Create React App example
+          Azure POC Shopping List App
         </Typography>
-        <ProTip />
         <Copyright />
       </Box>
     </Container>
